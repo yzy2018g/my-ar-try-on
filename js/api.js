44a8cd6882b1ad45363321087cdea292bfe5908a
MichaelYo <yzy2018g@gmail.com>
2026-06-03 23:19:46 +0800

    Enhance removeBackground with error handling
    Refactor removeBackground to include error handling and logging.

--- js/api.js
+++ js/api.js
@@ -3,13 +3,42 @@ import { Client } from "https://cdn.jsdelivr.net/npm/@gradio/client@1/dist/index
 let client = null;
 
 export async function initAPI() {
-    client = await Client.connect("michaelyo/my-ar-cloth-api");
+
+    console.log("INIT API");
+
+    client = await Client.connect(
+        "michaelyo/my-ar-cloth-api"
+    );
+
+    console.log("CLIENT CONNECTED");
 }
 
 export async function removeBackground(imageFile) {
-    if (!client) await initAPI();
 
-    return await client.predict("/predict", {
-        input_image: imageFile
-    });
+    try {
+
+        if (!client) {
+            console.log("NO CLIENT -> CONNECT");
+            await initAPI();
+        }
+
+        console.log("START PREDICT");
+
+        const result = await client.predict(
+            "/predict",
+            {
+                input_image: imageFile
+            }
+        );
+
+        console.log("PREDICT OK");
+
+        return result;
+
+    } catch(err) {
+
+        console.error("API ERROR", err);
+
+        throw err;
+    }
 }
